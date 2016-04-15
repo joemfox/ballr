bfbc729ddf9da865fb385b4576f6189d60643c75
Joe Fox <joe.m.fox@gmail.com>
2016-04-14 17:37:11 -0700

    remove alpha thing

--- hex_chart.R
+++ hex_chart.R
@@ -136,7 +136,7 @@ generate_hex_chart = function(hex_data, use_short_three = FALSE, metric = "bound
   base_court +
     geom_polygon(data = hex_data$hex_data,
                  aes_string(x = "adj_x", y = "adj_y", group = "hexbin_id",
-                            fill = metric, alpha = "hex_attempts"),
+                            fill = metric),
                  size = 0) +
          scale_fill_gradientn(paste0(fill_label, "   "),
                               colors = new_colors,
